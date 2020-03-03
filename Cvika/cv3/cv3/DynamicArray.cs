c65572b95ae82963c07db33e524fea1a6bf1bd98
michalscepka <scepkamichal1@gmail.com>
2020-03-03 16:05:31 +0100

    cv3

---
+++ Cvika/cv3/cv3/DynamicArray.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+
+namespace cv3
+{
+	class DynamicArray
+	{
+		private int?[] data;
+
+		public enum Info
+		{
+			Empty,
+			SomeData,
+			Full
+		}
+
+		public DynamicArray(int length = 5)
+		{
+			data = new int?[length];
+		}
+
+		public int Length
+		{
+			get
+			{
+				return data.Length;
+			}
+			set
+			{
+				if (value < data.Length)
+				{
+					throw new ApplicationException("New size small then before.");
+				}
+				Resize(value);
+			}
+		}
+
+		private void Resize(int new_size)
+		{
+			int?[] new_array = new int?[new_size];
+			Array.Copy(data, new_array, data.Length);
+			data = new_array;
+		}
+
+		public int? this[int index]
+		{
+			get
+			{
+				if (data.Length <= index)
+					return null;
+				return data[index];
+			}
+			set
+			{
+				if (data.Length <= index)
+					Resize(index + 1);
+				data[index] = value;
+			}
+		}
+
+		public override string ToString()
+		{
+			return string.Join(", ", data.Select(x => x.HasValue ? x.Value : 0));
+		}
+
+		public void Sum(ref int result)
+		{
+			foreach(int? value in data)
+			{
+				result += value.HasValue ? value.Value : 0;
+			}
+		}
+
+		public Info Inf
+		{
+			get
+			{
+				return Info.Empty;
+			}
+		}
+	}
+}
---
+++ Cvika/cv3/cv3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace cv3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			DynamicArray array = new DynamicArray();
+
+			Console.WriteLine(array.Length);
+			array.Length = 7;
+			Console.WriteLine(array.Length);
+
+			array[1] = 6;
+			array[8] = 9;
+			Console.WriteLine(array.Length);
+			Console.WriteLine(array);
+
+			int tmp = 0;
+			array.Sum(ref tmp);
+			Console.WriteLine(tmp);
+		}
+	}
+}
